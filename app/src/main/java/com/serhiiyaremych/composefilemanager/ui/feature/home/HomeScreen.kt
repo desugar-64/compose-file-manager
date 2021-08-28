92db94cec3aaf3d81462d8a37a1262a7569c4d23
sergeyfitis <sergeyfitis@gmail.com>
2021-08-28 22:01:07 +0300

    start working on bottom sheet

--- app/src/main/java/com/serhiiyaremych/composefilemanager/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/serhiiyaremych/composefilemanager/ui/feature/home/HomeScreen.kt
@@ -1,21 +1,31 @@
 package com.serhiiyaremych.composefilemanager.ui.feature.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import com.serhiiyaremych.composefilemanager.R
 import com.serhiiyaremych.composefilemanager.ui.theme.ComposeFileManagerTheme
+import com.serhiiyaremych.composefilemanager.ui.theme.Shapes
+import kotlin.math.roundToInt
+
+private enum class RecentFileSheetState { INITIAL, HALF_OPENED, EXPANDED }
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
@@ -23,6 +33,8 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     contentPadding: Dp = 16.dp
 ) {
+    var sheetTopOffset by remember { mutableStateOf(IntOffset.Zero) }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -39,72 +51,111 @@ fun HomeScreen(
 
         Spacer(modifier = Modifier.requiredHeight(32.dp))
 
-        // TODO: make own data source for resource tiles
-        val images = stringResource(R.string.images)
-        val videos = stringResource(R.string.videos)
-        val music = stringResource(R.string.music)
-        val apps = stringResource(R.string.apps)
-        val zipFiles = stringResource(R.string.zip_files)
-        val documents = stringResource(R.string.document)
-        val downloads = stringResource(R.string.downloads)
-        val add = stringResource(R.string.add)
-        val tiles = remember {
-            listOf(
-                ResourceTile.Images(images, Icons.Rounded.Image, Color(0xFF673AB7)),
-                ResourceTile.Videos(videos, Icons.Rounded.VideoLibrary, Color(0xFFF44336)),
-                ResourceTile.Music(music, Icons.Rounded.LibraryMusic, Color(0xFFFF9800)),
-                ResourceTile.Apps(apps, Icons.Rounded.Apps, Color(0xFF03A9F4)),
-                ResourceTile.ZipFiles(zipFiles, Icons.Rounded.Archive, Color(0xFF838383)),
-                ResourceTile.Documents(documents, Icons.Rounded.ListAlt, Color(0xFF1C70B3)),
-                ResourceTile.Downloads(downloads, Icons.Rounded.Download, Color(0xFF009688)),
-                ResourceTile.Add(add, Icons.Rounded.Add, Color(0xFF03A9F4)),
-            ).chunked(4)
+        UserCommonDataTypeTiles(contentPadding) { layoutPosition ->
+            sheetTopOffset = layoutPosition
         }
+    }
 
-        Column(
+    if (sheetTopOffset != IntOffset.Zero) {
+        val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+        val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
+        val sheetPeekY = sheetTopOffset.y.toFloat()
+        val sheetHalfOpenedY = sheetPeekY / 2
+        val sheetExpandedY = 0.0f
+
+        val swipeableState = rememberSwipeableState(initialValue = RecentFileSheetState.INITIAL)
+
+        val recentSheetSwipeAnchors = mapOf(
+            sheetPeekY to RecentFileSheetState.INITIAL,
+            sheetHalfOpenedY to RecentFileSheetState.HALF_OPENED,
+            sheetExpandedY to RecentFileSheetState.EXPANDED
+        )
+
+        val swipingOffset by swipeableState.offset
+        val swipeProgress = 1.0f - (swipingOffset / sheetPeekY).coerceIn(0.0f, 1.0f)
+        val sheetCornerRadius = lerp(16.dp, 0.dp, swipeProgress)
+        Surface(
+            shape = Shapes.medium.copy(CornerSize(sheetCornerRadius)),
             modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = contentPadding)
+                .requiredSize(screenWidth, screenHeight)
+                .offset { IntOffset(x = 0, y = swipingOffset.roundToInt()) }
+                .swipeable(
+                    state = swipeableState,
+                    anchors = recentSheetSwipeAnchors,
+                    orientation = Orientation.Vertical
+                ),
+            elevation = lerp(0.dp, 16.dp, swipeProgress)
         ) {
-            tiles.forEachIndexed { index, row ->
-                if (index > 0) {
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-                SpaceBetweenItem {
-                    row.forEach { tile ->
-                        ResourceTile(
-                            tile = tile,
-                            showDashBorder = tile is ResourceTile.Add,
-                            onClick = {})
-                    }
+            Column(modifier = Modifier.padding(contentPadding)) {
+                Text(
+                    text = "Recent files",
+                    style = MaterialTheme.typography.h6.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.onSurface
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserCommonDataTypeTiles(contentPadding: Dp, onLayoutBottomPlaced: (IntOffset) -> Unit) {
+    // TODO: make own data source for resource tiles
+    val images = stringResource(R.string.images)
+    val videos = stringResource(R.string.videos)
+    val music = stringResource(R.string.music)
+    val apps = stringResource(R.string.apps)
+    val zipFiles = stringResource(R.string.zip_files)
+    val documents = stringResource(R.string.document)
+    val downloads = stringResource(R.string.downloads)
+    val add = stringResource(R.string.add)
+    val tiles = remember {
+        listOf(
+            ResourceTile.Images(images, Icons.Rounded.Image, Color(0xFF673AB7)),
+            ResourceTile.Videos(videos, Icons.Rounded.VideoLibrary, Color(0xFFF44336)),
+            ResourceTile.Music(music, Icons.Rounded.LibraryMusic, Color(0xFFFF9800)),
+            ResourceTile.Apps(apps, Icons.Rounded.Apps, Color(0xFF03A9F4)),
+            ResourceTile.ZipFiles(zipFiles, Icons.Rounded.Archive, Color(0xFF838383)),
+            ResourceTile.Documents(documents, Icons.Rounded.ListAlt, Color(0xFF1C70B3)),
+            ResourceTile.Downloads(downloads, Icons.Rounded.Download, Color(0xFF009688)),
+            ResourceTile.Add(add, Icons.Rounded.Add, Color(0xFF03A9F4)),
+        ).chunked(4)
+    }
+
+    val currentLayoutPlaceListener by rememberUpdatedState(newValue = onLayoutBottomPlaced)
+
+    val contentPaddingPx = with(LocalDensity.current) { contentPadding.roundToPx() }
+
+    Column(
+        modifier = Modifier
+            .wrapContentHeight(align = Alignment.Top)
+            .padding(horizontal = contentPadding)
+            .onGloballyPositioned { layoutCoordinates: LayoutCoordinates ->
+                val offset = layoutCoordinates.localToRoot(Offset.Zero)
+                currentLayoutPlaceListener.invoke(
+                    IntOffset(
+                        x = 0,
+                        y = offset.y.roundToInt() + layoutCoordinates.size.height + contentPaddingPx
+                    )
+                )
+            }
+    ) {
+        tiles.forEachIndexed { index, row ->
+            if (index > 0) {
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            SpaceBetweenItem {
+                row.forEach { tile ->
+                    ResourceTile(
+                        tile = tile,
+                        showDashBorder = tile is ResourceTile.Add,
+                        onClick = {})
                 }
             }
-
         }
 
-
-        /*val swipeableState = rememberSwipeableState(initialValue = "A")
-        val anchors = mapOf(
-            160.0f to "A"
-        )
-        Box(
-            modifier =  Modifier
-                .offset { IntOffset(0, swipeableState.offset.value.roundToInt()) }
-                .fillMaxWidth()
-                .background(
-                    color = MaterialTheme.colors.onBackground,
-                    shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
-                )
-                .fillMaxHeight()
-                .swipeable(
-                    swipeableState,
-                    anchors = anchors,
-                    orientation = Orientation.Vertical,
-                    thresholds = { _, _ -> FractionalThreshold(0.5f) })
-
-        ) {
-
-        }*/
     }
 }
 
