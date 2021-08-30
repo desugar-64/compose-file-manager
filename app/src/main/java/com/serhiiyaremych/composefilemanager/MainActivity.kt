930db7fff9d3e435911bf44136fbd12de3710145
sergeyfitis <sergeyfitis@gmail.com>
2021-08-30 08:57:41 +0300

    working with storage

--- app/src/main/java/com/serhiiyaremych/composefilemanager/MainActivity.kt
+++ app/src/main/java/com/serhiiyaremych/composefilemanager/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.serhiiyaremych.composefilemanager
 
+import android.content.ContentUris
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
@@ -20,7 +25,6 @@ import com.serhiiyaremych.composefilemanager.ext.darken
 import com.serhiiyaremych.composefilemanager.ext.lighten
 import com.serhiiyaremych.composefilemanager.ui.feature.home.HomeScreen
 import com.serhiiyaremych.composefilemanager.ui.theme.Color1
-import com.serhiiyaremych.composefilemanager.ui.theme.Color2
 import com.serhiiyaremych.composefilemanager.ui.theme.ComposeFileManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,6 +60,60 @@ class MainActivity : ComponentActivity() {
                     HomeScreen()
                 }
             }
+        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            startActivity(Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION))
+//        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            val requestPemissions =
+//                registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { isGranted ->
+//                    // TODO
+//                }
+//            requestPemissions.launch()
+//        } else { }
+
+        val collection = MediaStore.Files.getContentUri("external")
+
+        val projection = arrayOf(
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.Files.FileColumns.DISPLAY_NAME,
+            MediaStore.Files.FileColumns.DATA,
+            MediaStore.Files.FileColumns.SIZE
+        )
+
+        val selection = ""
+
+        val cursor = contentResolver.query(
+            collection,
+            projection,
+            selection,
+            null,
+            null
+        )
+
+        if (cursor != null) {
+
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID)
+            val nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DISPLAY_NAME)
+            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE)
+            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA)
+
+            while (cursor.moveToNext()) {
+                // Get values of columns for a given video.
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val size = cursor.getInt(sizeColumn)
+                val data = cursor.getString(dataColumn)
+
+                val contentUri: Uri = ContentUris.withAppendedId(collection, id)
+
+                Log.d("FILES", "File found: { id: $id, name: $name, size: $size, data: $data }")
+            }
+
+
+            cursor.close()
         }
     }
 }
